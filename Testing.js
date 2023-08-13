b310bc4b4790a43d6b1ceefe62a046bb359d2ef0
anshpu <anshpuri28@gmail.com>
2023-08-13 19:34:21 -0400

    uodate

--- Testing.js
+++ Testing.js
@@ -1,11 +1,12 @@
 
 assert = chai.assert;
 
-describe('Testing function generateNum(upTo) of Task 2', function () {
+ddescribe('Background color test', function () {
 
-    it('Test 1: boundary value 1 for upTo', () => {
-        assert.equal(generateNum(1), 1);
-    });
+    it('Should have a white background', function () {
+        const myDiv = document.getElementById('myDiv');
+        const computedStyle = window.getComputedStyle(myDiv);
+        expect(computedStyle.backgroundColor).to.equal('rgb(255, 255, 255)');
 
     it('Test 2: generateNum(5) returns >= 1', () => {
         for (var i = 1; i <= 1000; i++) {
